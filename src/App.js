4c09a9e9c7777b656dbc40bdf99b0b59f23abc1c
Rishav Saha <rishavsaha@192.168.1.12>
2023-05-23 22:47:12 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,55 +1,73 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState, useEffect } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
+import Values from "values.js";
 
 function App() {
-  const [color, setColor] = useState('')
-  const [error, setError] = useState(false)
-  const [list, setList] = useState(new Values('#f15025').all(10))
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values("#a07e7e").all(10));
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      let colors = new Values(color).all(10)
-      setList(colors)
+      let colors = new Values(color).all(10);
+      setList(colors);
     } catch (error) {
-      setError(true)
-      console.log(error)
+      setError(true);
+      console.log(error);
     }
-  }
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setError(false);
+    }, 200);
+    return () => clearTimeout(timeout);
+  }, [color]);
+
+  useEffect(() => {
+    document.getElementById("next").style.backgroundColor = "black";
+  }, []);
 
   return (
     <>
-      <section className='container'>
+      <section className="container">
         <h3>color generator</h3>
+        <input
+          id="next"
+          type="color"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+          style={{ backgroundColor: color, width: 42, height: 42, padding: 0 }}
+        />
+        <div class="color-preview" style={{ backgroundColor: color }}></div>
         <form onSubmit={handleSubmit}>
           <input
-            type='text'
+            type="text"
             value={color}
             onChange={(e) => setColor(e.target.value)}
-            placeholder='#f15025'
-            className={`${error ? 'error' : null}`}
+            placeholder="#a07e7e"
+            className={error && "error"}
           />
-          <button className='btn' type='submit'>
+          <button
+            className="btn"
+            type="submit"
+            style={{ backgroundColor: color }}
+          >
             submit
           </button>
         </form>
       </section>
-      <section className='colors'>
+      <section className="colors">
         {list.map((color, index) => {
           return (
-            <SingleColor
-              key={index}
-              {...color}
-              index={index}
-              hexColor={color.hex}
-            />
-          )
+            <SingleColor key={index} {...color} index={index} hex={color.hex} />
+          );
         })}
       </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,31 +1,33 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
 
-const SingleColor = ({ rgb, weight, index, hexColor }) => {
-  const [alert, setAlert] = useState(false)
-  const bcg = rgb.join(',')
-  const hex = rgbToHex(...rgb)
-  const hexValue = `#${hexColor}`
+const SingleColor = ({ rgb, weight, hex, index }) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(",");
+  // const [a, b, c] = rgb;
   useEffect(() => {
     const timeout = setTimeout(() => {
-      setAlert(false)
-    }, 3000)
-    return () => clearTimeout(timeout)
-  }, [alert])
+      setAlert(false);
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
+  // const hex= rgbToHex(a,b,c);
+  const hexValue = "#" + hex;
   return (
     <article
-      className={`color ${index > 10 && 'color-light'}`}
+      className={index > 10 ? "color-light" : "color"}
       style={{ backgroundColor: `rgb(${bcg})` }}
       onClick={() => {
-        setAlert(true)
-        navigator.clipboard.writeText(hexValue)
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
       }}
     >
-      <p className='percent-value'>{weight}%</p>
-      <p className='color-value'>{hexValue}</p>
-      {alert && <p className='alert'>copied to clipboard</p>}
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
     </article>
-  )
-}
+  );
+};
 
-export default SingleColor
+export default SingleColor;
